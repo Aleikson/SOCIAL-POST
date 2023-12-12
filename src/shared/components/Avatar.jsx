ec8cc7a16585922816cdddefbf9ba3b33da0f000
Aleikson <aleiksonsilva@hotmail.com>
2023-12-11 22:15:15 -0300

    Added shared components

---
+++ src/shared/components/Avatar.jsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+const Avatar = (props) => {
+  const AvatarContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `;
+  const AvatarImage = styled.img`
+    display: block;
+    border-radius: 50%;
+    width: ${(props) => props.width || '100%'};
+    height: ${(props) => props.height || '100%'};
+    object-fit: cover;
+  `;
+
+  return (
+    <AvatarContainer
+      className={`avatar ${props.className}`}
+      style={props.style}
+    >
+      <AvatarImage
+        src={props.image}
+        alt={props.alt}
+        style={{ width: props.width, height: props.height }}
+      />
+    </AvatarContainer>
+  );
+};
+
+export default Avatar;
---
+++ src/shared/components/Card.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Card = (props) => {
+  const CardContainer = styled.div`
+    margin: 0;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    border-radius: 6px;
+    padding: 1rem;
+    overflow: hidden;
+    background: white;
+  `;
+
+  return (
+    <CardContainer className={`card ${props.className}`} style={props.style}>
+      {props.children}
+    </CardContainer>
+  );
+};
+
+export default Card;
