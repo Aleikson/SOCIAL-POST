cf9c55b0282c783d1b2cccce3d03a27fbb2b5a86
Aleikson <aleiksonsilva@hotmail.com>
2023-12-21 22:18:22 -0300

    Feat route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NewPlace from './places/pages/NewPlace';
 import Users from './user/pages/Users';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import UserPlaces from './places/pages/UserPlaces';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main>
         <Routes>
           <Route path='/' element={<Users />} />
+          <Route path='/:userId/places' element={<UserPlaces />} />
           <Route path='/place/new' element={<NewPlace />} />
         </Routes>
       </main>
