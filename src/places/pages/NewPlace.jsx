8a0d868954158aade72dff57d230ca3fd030996f
Aleikson <aleiksonsilva@hotmail.com>
2024-01-02 23:53:27 -0300

    Adding new place state

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,5 +1,30 @@
+import styled from 'styled-components';
+import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+
+const Form = styled.form`
+  list-style: none;
+  margin: 0 auto;
+  padding: 1rem;
+  width: 90%;
+  max-width: 40rem;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  border-radius: 6px;
+  background: white;
+`;
+
 const NewPlace = () => {
-  return <h2>NewPlace</h2>;
+  return (
+    <Form action='' className='placeForm'>
+      <Input
+        element='input'
+        type='text'
+        label='Title'
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText='Please enter a valid title'
+      />
+    </Form>
+  );
 };
 
 export default NewPlace;
