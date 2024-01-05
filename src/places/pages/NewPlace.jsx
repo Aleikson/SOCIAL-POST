18094de0d0af1ab5ef7d61a4dd1638e712207d1a
Aleikson <aleiksonsilva@hotmail.com>
2024-01-04 22:41:37 -0300

    Feat managing form state

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import Input from '../../shared/components/FormElements/Input';
 import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import { useCallback, useReducer } from 'react';
+import Button from '../../shared/components/FormElements/Button';
 
 const Form = styled.form`
   list-style: none;
@@ -13,16 +15,89 @@ const Form = styled.form`
   background: white;
 `;
 
+const formReducer = (state, action) => {
+  let formIsValid;
+  switch (action.type) {
+    case 'INPUT_CHANGE':
+      formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid,
+      };
+    default:
+      return state;
+  }
+};
+
 const NewPlace = () => {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: '',
+        isValid: false,
+      },
+      description: {
+        value: '',
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: 'INPUT_CHANGE',
+      value: value,
+      isValid: isValid,
+      inputId: id,
+    });
+  }, []);
+
+  const placeSubmitHandler = event => {
+    event.preventDefault() 
+  }
+
   return (
-    <Form action='' className='placeForm'>
+    <Form onSubmit={placeSubmitHandler}>
       <Input
+        id='title'
         element='input'
         type='text'
         label='Title'
         validators={[VALIDATOR_REQUIRE()]}
         errorText='Please enter a valid title'
+        onInput={inputHandler}
       />
+      <Input
+        id='address'
+        element='input'
+        label='Address'
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText='Please enter a valid address'
+        onInput={inputHandler}
+      />
+      <Input
+        id='description'
+        element='textarea'
+        label='description'
+        validators={[VALIDATOR_REQUIRE(5)]}
+        errorText='Please enter a valid description'
+        onInput={inputHandler}
+      />
+      <Button type='submit' disabled={!formState.isValid}>
+        ADD PLACE
+      </Button>
     </Form>
   );
 };
